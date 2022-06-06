4741f20933cb53fbd7e33ecc85565d85abc3da1a
arupalakka <mochfhaiz18@gmail.com>
2022-06-06 14:00:58 +0800

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,10 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ratione officia voluptate quasi dolores veritatis voluptates, perspiciatis est repellat illo, animi praesentium beatae eum tempore aliquid. Libero nihil hic maiores explicabo.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+        <li>tiga</li>
+    </ul>
 </body>
 </html>
